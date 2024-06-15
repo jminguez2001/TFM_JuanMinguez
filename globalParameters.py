778f0f722792a2c80d7eab966d98ec7e6f4b269b
jminguez2001 <jminguez2001@gmail.com>
2024-06-15 09:28:50 +0200

    Update

--- globalParameters.py
+++
@@ -1,4 +0,0 @@
-# ENVIRONMENT: PRO, PRE, DEV
-ENVIRONMENT = 'DEV'
-path = './ProyectLogs'
-params = {"WLSACCESSID" : '5cbfde7e-b7bd-44e3-9e2d-fb335f5a2deb', "WLSSECRET" : 'f526471f-ed2c-462d-a933-1241228cd704', "LICENSEID" : 2489204 }
